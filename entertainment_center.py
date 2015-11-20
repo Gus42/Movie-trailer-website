4233aa5288d6b547560f8db20535ab3bcb41861a
Gus42 <gus815@gmail.com>
2015-11-20 12:49:42 +0000

    Fix: follow Python rule
    Blank space, new line etc.

--- entertainment_center.py
+++ entertainment_center.py
@@ -16,37 +16,40 @@ toy_story = media.Movie("Toy Story",
                         "John Lasseter")
 avatar = media.Movie("Avatar",
                      "Blue aliens hate pink aliens, they have good reasons",
-                     "http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd"
-                     +"8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp",
+                     ("http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2"
+                      "cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp"),
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY",
                      "James Cameron")
 interstellar = media.Movie("Interstellar",
-                        "A man go in space trying to save humanity",
-                        "https://lh5.googleusercontent.com/-_LZCNOpWcfI/AAAAAAA"
-                        +"AAAI/AAAAAAAAAAA/SEQ-XjwXhlU/s0-c-k-no-ns/photo.jpg",
-                        "https://www.youtube.com/watch?v=zSWdZVtXT7E",
-                        "Christopher Nolan")
+                           "A man go in space trying to save humanity",
+                           ("https://lh5.googleusercontent.com/-_LZCNOpWcfI/"
+                            "AAAAAAAAAAI/AAAAAAAAAAA/SEQ-XjwXhlU/s0-c-k-no-"
+                            "ns/photo.jpg"),
+                           "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+                           "Christopher Nolan")
 gladiator = media.Movie("Gladiator",
                         "A General become a gladiator",
-                        "https://upload.wikimedia.org/wikipedia/en/8/8d/"
-                        +"Gladiator_ver1.jpg",
+                        ("https://upload.wikimedia.org/wikipedia/en/8/8d/"
+                         "Gladiator_ver1.jpg"),
                         "https://www.youtube.com/watch?v=IvTT29cavKo",
                         "Ridley Scott")
 cloud_atlas = media.Movie("Cloud Atlas",
-                        "Different people in different time trying to be free",
-                        "http://t0.gstatic.com/images?q=tbn:ANd9GcQZQhas"
-                        +"O56in_vxYDa30rB9tzQg1wk6yKJkkdVoxG43hCJeYuEm",
-                        "https://www.youtube.com/watch?v=hWnAqFyaQ5s",
-                        "Lana Wachowski, Andy Wachowski, Tom Tykwer")
+                          "People of different time trying to be free",
+                          ("http://t0.gstatic.com/images?q=tbn:ANd9GcQZQhas"
+                           "O56in_vxYDa30rB9tzQg1wk6yKJkkdVoxG43hCJeYuEm"),
+                          "https://www.youtube.com/watch?v=hWnAqFyaQ5s",
+                          "Lana Wachowski, Andy Wachowski, Tom Tykwer")
 gran_torino = media.Movie("Gran Torino",
-                        "An old man and his Gran Torino",
-                        "http://www.gstatic.com/tv/thumb/movieposters/"
-                        +"183329/p183329_p_v7_ab.jpg",
-                        "https://www.youtube.com/watch?v=9ecW-d-CBPc",
-                        "Clint Eastwood")
+                          "An old man and his Gran Torino",
+                          ("http://www.gstatic.com/tv/thumb/movieposters/"
+                           "183329/p183329_p_v7_ab.jpg"),
+                          "https://www.youtube.com/watch?v=9ecW-d-CBPc",
+                          "Clint Eastwood")
 
 # Store the instances of Movie in a list
 movies = [toy_story, avatar, interstellar, gladiator, cloud_atlas, gran_torino]
+
 # Call the open_movies_page function of fresh_tomatoes.py.
-# It will take the list of movies, and it will generate a web page to show them.
+# It will take the list of movies, and it will generate a web page to show them
 fresh_tomatoes.open_movies_page(movies)
+
--- media.py
+++ media.py
@@ -1,9 +1,11 @@
 import webbrowser
 
-#create class Movie
+
+# Create class Movie
 class Movie(object):
     """
-    Class Movie has a constructor with title, storyline, poster, trailer, director of the movie.
+    Class Movie has a constructor with title, storyline,
+    poster, trailer and director of the movie.
     And it has a function show_trailer.
     """
     def __init__(self, title, storyline, poster, trailer, director):
@@ -12,6 +14,7 @@ class Movie(object):
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
         self.director = director
-    #function that open the webbrowser to the specified URL
+
+    # Function that open the webbrowser to the specified URL
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
